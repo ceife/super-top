984de6d246999c62096b0047c702cb8848709bf3
Felipe Dacs <felipe.dacs@gmail.com>
2018-06-28 11:07:36 -0300

    criação do diagrama de classe

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,21 @@ export default {
     criaWS(nome){
       let rota = this.$router;
       this.websocket = new WebSocket('ws://172.29.80.15:8080/StopWeb/websocket?nome=' + nome);
+      this.escutaWS();
+      // this.websocket.onmessage = function(msg){
+      //   // console.log(msg.data);
+      //
+      //   console.log(JSON.parse(msg.data));
+      //
+      //   let resposta = JSON.parse(msg.data);
+      //   if(resposta.funcao == "newRodada"){
+      //     this.$router.push({ name: 'jogo'});
+      //   } else if (true) {
+      //     rota.push({ name: 'jogo'});
+      //   }
+      // }
+    },
+    escutaWS(){
       this.websocket.onmessage = function(msg){
         // console.log(msg.data);
 
--- src/components/jogo/jogo.vue
+++ src/components/jogo/jogo.vue
@@ -5,35 +5,32 @@
       <formulario @responde='responde' :categorias='categorias' :indisponivel='indisponivel' :letra='letra'>
       </formulario>
 
-      <sidebarLetra :letra='letra'>
-      </sidebarLetra>
-      <sidebarFrame>
-        <ul class='li-jogadores'>
-          <li v-for='jogador in jogadores'><sidebarBox :jogador='jogador'></sidebarBox></li>
-        </ul>
-      </sidebarFrame>
+      <letraBox :letra='letra'>
+      </letraBox>
+
+      <jogadoresList :jogadores='jogadores'>
+      </jogadoresList>
+
       <!--<pre>{{categorias}}</pre>-->
     </div>
   </div>
 </template>
 
 <script>
-import sidebarFrame from '../shared/sidebar/sidebarFrame.vue';
-import sidebarBox from '../shared/sidebar/sidebarBox.vue';
+import jogadoresList from './jogadoresList.vue';
 import formulario from './formulario.vue';
-import sidebarLetra from './sidebarLetra.vue';
+import letraBox from './letraBox.vue';
 export default {
   props: ['websocket'],
   components: {
-    'sidebarFrame': sidebarFrame,
+    'jogadoresList': jogadoresList,
     'formulario': formulario,
-    'sidebarBox': sidebarBox,
-    'sidebarLetra': sidebarLetra
+    'letraBox': letraBox
   },
   //name: 'app',
   data () {
     return {
-      padrao:{
+      padraoResposta:{
         "funcao": "putRespostas",
         "data": [
           "categoria1",
@@ -61,7 +58,32 @@ export default {
   },
   created() {
     //dados mockados
-    this.jogadores = [{nome:'felipe',pontos:25},{nome:'jennifer',pontos:36},{nome:'jennifer',pontos:36},{nome:'jennifer',pontos:36},{nome:'jennifer',pontos:36},{nome:'jennifer',pontos:36}];
+    this.jogadores = [
+      {
+        nome:'felipe',
+        pontos:25
+      },
+      {
+        nome:'jennifer',
+        pontos:36
+      },
+      {
+        nome:'jennifer',
+        pontos:36
+      },
+      {
+        nome:'jennifer'
+        ,pontos:36
+      },
+      {
+        nome:'jennifer',
+        pontos:36
+      },
+      {
+        nome:'jennifer',
+        pontos:36
+      }
+    ];
     this.categorias = [
       {
         'nome':'nome',
@@ -111,48 +133,19 @@ export default {
         valor:['um','dois','um','dois','um']
       }));
     }
-
   }
 }
 </script>
 
 <style scoped>
-.li-jogadores li:nth-child(1) div,.li-jogadores li:nth-child(2) div,.li-jogadores li:nth-child(3) div {
-  color: white;
-  font-weight: bold;
-  text-shadow: 0px 1px 1px #4d4d4d;
-}
-.li-jogadores li:nth-child(1) div {
-  background-color: gold;
-}
-.li-jogadores li:nth-child(2) div {
-  background-color: silver;
-}
-.li-jogadores li:nth-child(3) div {
-  background-color: #CD7F32;
-}
 
-
-.li-jogadores li div:nth-child(an+b){
-  background-color: red;
-}
 .container{
   margin: 2%;
   max-width: none;
   width: auto;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-  min-width: 100%;
-}
 
-li {
-  display: inline-block;
-  margin: 0;
-  min-width: 100%;
-}
 
 a {
   color: #42b983;
